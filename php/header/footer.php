292169e34e69f81ba28e8a32c66c6a14b627791a
Meor Danish <danishfarhan700@gmail.com>
2025-06-09 19:01:55 +0800

    Add dynamic linking to footer

--- php/header/footer.php
+++ php/header/footer.php
@@ -1,8 +1,23 @@
+<?php
+function getBasePath()
+{
+  // Determine if we're on localhost or production
+  $isLocalhost = ($_SERVER['HTTP_HOST'] === 'localhost' || strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false);
+
+  if ($isLocalhost) {
+    // On localhost, we're in a subdirectory
+    return '/sesta-registration/';
+  } else {
+    // On production, we're at the root
+    return '/';
+  }
+}
+?>
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
-<link href="footerStyle.css" rel="stylesheet" />
+<link href="<?php echo getBasePath(); ?>css/footerStyle.css" rel="stylesheet" />
 
 </head>
 <body>
