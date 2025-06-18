2f1a2e73f4146f28ea440d2937815c6d9e852212
Meor Danish <danishfarhan700@gmail.com>
2025-06-18 21:58:45 +0800

    Fix full report search

--- php/admin/teacherSearchReport.php
+++ php/admin/teacherSearchReport.php
@@ -6,11 +6,8 @@
 if (!function_exists("GetSQLValueString")) {
 function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
 {
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
-
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+  global $con;
+  $theValue = mysqli_real_escape_string($con, $theValue);
 
   switch ($theType) {
     case "text":
