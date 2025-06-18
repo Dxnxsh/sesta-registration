83f22d2ce56735921ec2c21c71ea1883f81b8fb6
Meor Danish <danishfarhan700@gmail.com>
2025-06-18 13:48:17 +0800

    Update pdf_summary (admin)

--- php/admin/pdf_summary.php
+++ php/admin/pdf_summary.php
@@ -70,9 +70,19 @@
 
 <?php                
  
+include_once('../../tcpdf/tcpdf_autoconfig.php');
 include_once('../../tcpdf/tcpdf.php');
 
-	$inv_mst_data_row = mysqli_fetch_array($inv_mst_results, MYSQLI_ASSOC);
+	// Define missing TCPDF constants if not already defined
+	if (!defined('PDF_HEADER_TITLE')) define('PDF_HEADER_TITLE', 'School System Summary');
+	if (!defined('PDF_HEADER_STRING')) define('PDF_HEADER_STRING', 'System Summary Report');
+	if (!defined('PDF_MARGIN_FOOTER')) define('PDF_MARGIN_FOOTER', 15);
+	if (!defined('PDF_MARGIN_LEFT')) define('PDF_MARGIN_LEFT', 15);
+	if (!defined('PDF_MARGIN_RIGHT')) define('PDF_MARGIN_RIGHT', 15);
+	if (!defined('PDF_FONT_NAME_MAIN')) define('PDF_FONT_NAME_MAIN', 'helvetica');
+	if (!defined('PDF_FONT_SIZE_MAIN')) define('PDF_FONT_SIZE_MAIN', 12);
+	if (!defined('PDF_FONT_NAME_DATA')) define('PDF_FONT_NAME_DATA', 'helvetica');
+	if (!defined('PDF_FONT_SIZE_DATA')) define('PDF_FONT_SIZE_DATA', 8);
 
 	//----- Code for generate pdf
 	$pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
