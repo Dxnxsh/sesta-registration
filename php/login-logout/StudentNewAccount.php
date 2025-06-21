8763fce3c5469341936025778955be90efa3b7ae
Meor Danish <danishfarhan700@gmail.com>
2025-06-21 10:47:48 +0800

    Improve error handling for facereg modal

--- php/login-logout/StudentNewAccount.php
+++ php/login-logout/StudentNewAccount.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="../../css/modal.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <title>Register Student</title>
 </head>
 
